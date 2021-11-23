ad230eae2e5a89dc50b59905a2761c05cb37526e
Peter Mills <peter.mills@truelayer.com>
2021-11-23 21:59:18 +0000

    Remove unused imports

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use sqlx::postgres::PgPoolOptions;
-use std::net::TcpListener;
+
+
 use zero2prod::configuration::get_configuration;
-use zero2prod::email_client::EmailClient;
+
 use zero2prod::startup::Application;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -1,9 +1,9 @@
 use once_cell::sync::Lazy;
 use sqlx::{Connection, Executor, PgConnection, PgPool};
-use std::net::TcpListener;
+
 use uuid::Uuid;
 use zero2prod::configuration::{get_configuration, DatabaseSettings};
-use zero2prod::email_client::EmailClient;
+
 use zero2prod::startup::{get_connection_pool, Application};
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
