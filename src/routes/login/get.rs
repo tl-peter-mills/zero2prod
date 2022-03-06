35f5af6c7997bd0f56f32c99e6415e7c29d69dc7
Peter Mills <peter.mills@truelayer.com>
2022-03-06 09:47:08 +0000

    Login auth errors with hmac tag

--- src/routes/login/get.rs
+++ src/routes/login/get.rs
@@ -1,8 +1,50 @@
 use actix_web::http::header::ContentType;
-use actix_web::HttpResponse;
+use actix_web::{HttpResponse, web};
 
-pub async fn login_form() -> HttpResponse {
+#[derive(serde::Deserialize)]
+pub struct QueryParams {
+    error: Option<String>,
+}
+
+pub async fn login_form(query: web::Query<QueryParams>) -> HttpResponse {
+    let error_html = match query.0.error {
+        None => "".into(),
+        Some(error_message) => format!(
+            "<p><i>{}</i></p>",
+            htmlescape::encode_minimal(&error_message)
+        ),
+    };
     HttpResponse::Ok()
         .content_type(ContentType::html())
-        .body(include_str!("login.html"))
+        .body(format!(
+            r#"<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8">
+    <title>Login</title>
+</head>
+<body>
+    {}
+    <form action="/login" method="post">
+        <label>Username
+            <input
+                type="text"
+                placeholder="Enter username"
+                name="username"
+            />
+        </label>
+        <label>Username
+            <input
+                    type="password"
+                    placeholder="Enter password"
+                    name="password"
+            />
+        </label>
+
+        <button type="submit">Login</button>
+    </form>
+</body>
+</html>"#,
+            error_html
+        ))
 }
--- src/routes/login/post.rs
+++ src/routes/login/post.rs
@@ -1,6 +1,10 @@
 use actix_web::{web, HttpResponse};
 use reqwest::header::LOCATION;
-use secrecy::Secret;
+use secrecy::{Secret, ExposeSecret};
+use crate::authentication::{Credentials, validate_credentials, AuthError};
+use sqlx::PgPool;
+use crate::routes::error_chain_fmt;
+use hmac::Hmac;
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -8,8 +12,66 @@ pub struct FormData {
     password: Secret<String>,
 }
 
-pub async fn login(_form: web::Form<FormData>) -> HttpResponse {
-    HttpResponse::SeeOther()
-        .insert_header((LOCATION, "/"))
-        .finish()
+#[tracing::instrument(
+    skip(form, pool, secret)
+    fields(username=tracing::field::Empty, user_id=tracing::field::Empty)
+)]
+pub async fn login(
+    form: web::Form<FormData>,
+    pool: web::Data<PgPool>,
+    secret: web::Data<Secret<String>>,
+) -> HttpResponse {
+    let credentials = Credentials {
+        username: form.0.username,
+        password: form.0.password,
+    };
+    tracing::Span::current()
+        .record("username", &tracing::field::display(&credentials.username));
+
+    match validate_credentials(credentials, &pool).await {
+        Ok(user_id) => {
+            tracing::Span::current()
+                .record("user_id", &tracing::field::display(&user_id));
+            HttpResponse::SeeOther()
+                .insert_header((LOCATION, "/"))
+                .finish()
+        }
+        Err(e) => {
+            let e = match e {
+                AuthError::InvalidCredentials(_) => LoginError::AuthError(e.into()),
+                AuthError::UnexpectedError(_) => LoginError::UnexpectedError(e.into()),
+            };
+            let query_string = format!(
+                "error={}",
+                urlencoding::Encoded::new(e.to_string())
+            );
+            let hmac_tag = {
+                let mut mac = Hmac::<sha2::Sha256>::new_from_slice(
+                    secret.expose_secret().as_bytes()
+                ).unwrap();
+                mac.update(query_string.as_bytes());
+                mac.finalize().into_bytes()
+            };
+            HttpResponse::SeeOther()
+                .insert_header((
+                    LOCATION,
+                    format!("/login?{}&tag={:x}", query_string, hmac_tag)
+                ))
+                .finish()
+        }
+    }
+}
+
+#[derive(thiserror::Error)]
+pub enum LoginError {
+    #[error("Authentication failed")]
+    AuthError(#[source] anyhow::Error),
+    #[error("Something went wrong")]
+    UnexpectedError(#[from] anyhow::Error),
+}
+
+impl std::fmt::Debug for LoginError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        error_chain_fmt(self, f)
+    }
 }
